e3eda72464aba8507a066f70128a59af9c83f142
datou <1392767180@qq.com>
2019-05-29 00:47:31 +0800

    完成Header组件

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,30 @@
 <template>
   <div id="app">
+    <n-header></n-header>
     <router-view></router-view>
   </div>
 </template>
+<script>
+import Header from "./components/Header";
+
+export default {
+  components: {
+    "n-header": Header
+  }
+};
+</script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,103 @@
+<template>
+  <header :class="{login: isLogin, noLogin: !isLogin}">
+    <template v-if="isLogin">
+      <h1>Let's share</h1>
+      <h4>多人共享博客</h4>
+      <el-row>
+        <el-button
+          type="primary"
+          plain
+        >注册账号</el-button>
+        <el-button
+          type="primary"
+          plain
+        >立即登陆</el-button>
+      </el-row>
+    </template>
+    <template v-else-if="!isLogin">
+      <div class="headerContent">
+        <h1>Let's share</h1>
+        <div class="user">
+          <svg
+            class="icon"
+            aria-hidden="true"
+          >
+            <use xlink:href="#icon-Pencil1"></use>
+          </svg>
+          <img
+            src="../assets/avator.png"
+            alt="avator"
+          >
+        </div>
+      </div>
+    </template>
+  </header>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isLogin: false
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+header {
+  color: #fff;
+}
+header.login {
+  height: 260px;
+  background-color: rgba(64, 158, 255, 0.5);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  h1 {
+    font-size: 40px;
+    text-transform: uppercase;
+  }
+  h4 {
+    margin-top: 20px;
+    font-size: 16px;
+  }
+  .el-row {
+    margin-top: 60px;
+    .el-button {
+      margin: 0 10px 0 10px;
+    }
+  }
+}
+header.noLogin {
+  background-color: rgba(64, 158, 255, 0.5);
+  display: flex;
+  justify-content: center;
+  .headerContent {
+    width: 75%;
+    height: 64px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    h1 {
+      font-size: 30px;
+      text-transform: uppercase;
+    }
+    .user {
+      display: flex;
+      align-items: center;
+      .icon {
+        width: 2.2em;
+        height: 2.2em;
+        cursor: pointer;
+      }
+      img {
+        margin-left: 14px;
+        width: 2.6em;
+        border-radius: 50%;
+      }
+    }
+  }
+}
+</style>
+
+
