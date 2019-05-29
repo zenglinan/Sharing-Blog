7f4a65247926a2cd3e095a48aac60605bc1e383f
datou <1392767180@qq.com>
2019-05-29 16:14:34 +0800

    rename Login to Register component

--- src/views/Login/template.vue
+++ src/views/Login/template.vue
@@ -1,27 +1,8 @@
 <template>
-  <div class="loginWrapper">
-    <div class="login">
-      <div class="item">
-          <label>
-          用户名
-          <el-input placeholder="请输入用户名"></el-input>
-          <p>该账户可以注册</p>
-        </label>
-      </div>
-      <div class="item">
-        <label>
-          密码
-          <el-input placeholder="请输入密码" show-password></el-input>
-          <p>√</p>
-        </label>
-      </div>
-      <div class="loginButton">
-        <el-button type="primary">点我注册</el-button>
-        <el-button type="primary">直接登录</el-button>
-      </div>
-    </div>
-    
+  <div class="registerWrapper">
+    <h1>Register</h1>
+
   </div>
 </template>
 <script src="./template.js"></script>
-<style lang="scss" scoped src="./template.scss"></style>
+<style src="./template.css"></style>
--- src/views/Register/template.vue
+++ src/views/Register/template.vue
@@ -1,8 +1,29 @@
 <template>
-  <div class="registerWrapper">
-    <h1>Register</h1>
-
+  <div class="loginWrapper">
+    <div class="login">
+      <div class="item">
+        <label>
+          用户名
+          <el-input placeholder="请输入用户名"></el-input>
+          <p>该账户可以注册</p>
+        </label>
+      </div>
+      <div class="item">
+        <label>
+          密码
+          <el-input
+            placeholder="请输入密码"
+            show-password
+          ></el-input>
+          <p>√</p>
+        </label>
+      </div>
+      <div class="loginButton">
+        <el-button type="primary">点我注册</el-button>
+      </div>
+    </div>
+    <p class="loginIn"><span>已有账号？</span><span><a href="/login">直接登录</a></span></p>
   </div>
 </template>
 <script src="./template.js"></script>
-<style src="./template.css"></style>
+<style lang="scss" scoped src="./template.scss"></style>
