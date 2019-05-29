3d174ed1139f380acd928f45380df1be18c85a39
datou <1392767180@qq.com>
2019-05-29 13:46:30 +0800

    complete CSS of Index component

--- src/views/Index/template.vue
+++ src/views/Index/template.vue
@@ -1,8 +1,26 @@
 <template>
   <div class="indexWrapper">
-    <h1>Index</h1>
-
+    <div class="item">
+      <figure class="avatar">
+        <img src="../../assets/avator.png" alt="">
+        <figcaption>若愚</figcaption> 
+      </figure>
+      <article>
+        <h3>前端异步大揭秘 <span>3天前</span></h3> 
+        <p>本文以一个简单的文件读写为例，讲解了异步的不同写法，包括 普通的 callback、ES2016中的Promise和Generator、 Node 用于解决回调的co 模块、ES2017中的async/await。适合初步接触 Node.js以及少量 ES6语法的同学阅读...</p>
+      </article>
+    </div>
+    <div class="item">
+      <figure class="avatar">
+        <img src="../../assets/avator.png" alt="">
+        <figcaption>若愚</figcaption> 
+      </figure>
+      <article>
+        <h3>前端异步大揭秘 <span>3天前</span></h3> 
+        <p>本文以一个简单的文件读写为例，讲解了异步的不同写法，包括 普通的 callback、ES2016中的Promise和Generator、 Node 用于解决回调的co 模块、ES2017中的async/await。适合初步接触 Node.js以及少量 ES6语法的同学阅读...</p>
+      </article>
+    </div>
   </div>
 </template>
 <script src="./template.js"></script>
-<style src="./template.css"></style>
+<style lang="scss" src="./template.scss"></style>
