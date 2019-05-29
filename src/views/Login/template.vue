f01391a797bbdcc9e6ee7d5e0de82ada76cfe00c
datou <1392767180@qq.com>
2019-05-29 16:01:04 +0800

    update login component

--- src/views/Login/template.vue
+++ src/views/Login/template.vue
@@ -5,17 +5,23 @@
           <label>
           用户名
           <el-input placeholder="请输入用户名"></el-input>
+          <p>该账户可以注册</p>
         </label>
       </div>
       <div class="item">
         <label>
           密码
           <el-input placeholder="请输入密码" show-password></el-input>
+          <p>√</p>
         </label>
+      </div>
+      <div class="loginButton">
+        <el-button type="primary">点我注册</el-button>
+        <el-button type="primary">直接登录</el-button>
       </div>
     </div>
     
   </div>
 </template>
 <script src="./template.js"></script>
-<style lang="scss" src="./template.scss"></style>
+<style lang="scss" scoped src="./template.scss"></style>
